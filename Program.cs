893678b9a9cc212a9072dd8ba20e0f5e1c849cf7
Leandro <leandrogomesdonascimento@gmail.com>
2025-10-23 19:47:04 -0300

    Completando as modificações para cumprir o desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI654321", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
